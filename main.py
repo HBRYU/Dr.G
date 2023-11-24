1fdf69a87d6fadc118255498f83aca1bbef7e4a6
HBRYU <48374744+HBRYU@users.noreply.github.com>
2023-11-24 21:46:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 import tkinter as tk
 
+# pi = tk.PhotoImage
+#
+# root = tk.Tk()
+# root.geometry("300x200")
+#
+# # Frame for pack
+# pack_frame = tk.Frame(root)
+# pack_frame.pack(side="top", fill="both", expand=True)
+#
+# button1 = tk.Button(pack_frame, text="Packed Button", width=20)
+# button1.pack(pady=10)
+#
+# # Frame for grid
+# grid_frame = tk.Frame(root)
+# grid_frame.pack(side="top", fill="both", expand=True)
+#
+# entry = tk.Entry(grid_frame, width=20)
+# entry.grid(row=0, column=0, padx=10, pady=10)
+#
+# button2 = tk.Button(grid_frame, text="Grid Button")
+# button2.grid(row=0, column=1, padx=10)
+#
+# root.mainloop()
+#
+# # this is a comment
+
 pi = tk.PhotoImage
 
 root = tk.Tk()
-root.geometry("300x200")
+root.geometry("720x1280")
 
-# Frame for pack
-pack_frame = tk.Frame(root)
-pack_frame.pack(side="top", fill="both", expand=True)
+size_x, size_y = (720, 1280)
 
-button1 = tk.Button(pack_frame, text="Packed Button", width=20)
-button1.pack(pady=10)
 
-# Frame for grid
-grid_frame = tk.Frame(root)
-grid_frame.pack(side="top", fill="both", expand=True)
+class text_bubble:
+    def __init__(self, text):
+        self.text = text
+        self.position = self.get_new_position()
 
-entry = tk.Entry(grid_frame, width=20)
-entry.grid(row=0, column=0, padx=10, pady=10)
+    def get_new_position(self):
+        return (0, 0)
 
-button2 = tk.Button(grid_frame, text="Grid Button")
-button2.grid(row=0, column=1, padx=10)
 
-root.mainloop()
+top_label = tk.Label(root, text="Dr.G 도우미 상담",
+                     font="Ariel"
+                     bg.)
+
+
+
+
 
